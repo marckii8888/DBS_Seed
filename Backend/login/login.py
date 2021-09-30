e1bf22a16ef00e615adb744d3191c43027831bc6
winthony <54660753+winthony@users.noreply.github.com>
2021-09-30 12:01:34 +0800

    Created login and pulling of all customer db
    Uploaded db data and tested on postman to check if get request gets all db data

--- Backend/login/login.py
+++ Backend/login/login.py
@@ -1,10 +1,52 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from os import environ
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/ecommerce'
+db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/user'
+class Customer(db.Model):
+    __tablename__ = 'customer'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username  = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+    postal_code = db.Column(db.String(255), nullable=True)
+    gender = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.Date, nullable=False)
+
+    def __init__(self, id, username, first_name, last_name, postal_code, gender,created_at ):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.postal_code = postal_code
+        self.gender = gender
+        self.created_at = created_at 
+
+    def json(self):
+        return {"id": self.id, "username": self.username, "first_name": self.first_name, "last_name": self.last_name, "postal_code":self.postal_code, "gender":self.gender, "created_at": self.created_at}
+
+@app.route('/customer', methods=['GET'])
+def get_all():
+	return jsonify({"customer": [customer.json() for customer in Customer.query.all()]})
+
+# @app.route("/login", methods=["POST"])
+# def authenticate():
+#     data = request.get_json()
+#     email = data['email']
+#     password = data['password']
+
+#     user = User.query.filter_by(email=email).first()
+    
+#     if user:
+#         check_password = sha256_crypt.verify(password, user.password)
+#         if check_password:
+#             email = user.email
+#             return jsonify({"email":email}), 201
+
+#     return jsonify({"message": "Invalid email or password."}), 404
