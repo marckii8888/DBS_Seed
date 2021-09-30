82e9b290d3e814c14f04c219bb0e39ed4ce5aa56
umairaevelyn <umairaweee@hotmail.com>
2021-09-30 13:19:51 +0800

    HTML part of Shopping Cart

--- Frontend/src/components/ShoppingCart.js
+++ Frontend/src/components/ShoppingCart.js
@@ -3,7 +3,45 @@ import React from 'react';
 const ShoppingCart = () => {
 
     return (
-        <div>ShoppingCart</div>
+        <div class="row">
+            <label>
+				My Cart 
+			</label>
+            <div class="form-group col-sm-12 col-md-12">
+                <div class="col-sm-7 col-md-7">
+                    <table id="cartTable">
+                        <thead>
+                            <tr>
+                                <th>Quantity</th>
+                                <th>Item</th>
+                                <th>Price</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody id="cartDataBody">
+        
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-sm-5 col-md-5">
+                    <label>
+                        Total
+                    </label>
+                    <table id="totalTable">
+                        <thead>
+                            <tr>
+                                <th>Total Price</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody id="totalBody">
+        
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     )
 }
 
