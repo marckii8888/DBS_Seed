d48d3b8a944bdf2ae058647f641c2b6b881ede86
umairaevelyn <umairaweee@hotmail.com>
2021-09-30 14:48:20 +0800

    Update ShoppingCart.js

--- Frontend/src/components/ShoppingCart.js
+++ Frontend/src/components/ShoppingCart.js
@@ -1,26 +1,64 @@
 import React from 'react';
+import productlist from '../datajson/products.json'
 
 const ShoppingCart = () => {
+    const ShopList = {
+        "shopList":[
+            {
+                "name": "Detergent",
+                "quantity": 1,
+                "price": 4.99
+            },
+            {
+                "name": "Apples",
+                "quantity": 1,
+                "price": 5.95
+            },
+            {
+                "name": "White Bread",
+                "quantity": 1,
+                "price": 2.40
+            },
+            {
+                  "name": "Pork",
+                  "quantity": 1,
+                  "price": 6.70
+            },
+            {
+                  "name": "Celery",
+                  "quantity": 1,
+                  "price": 3.10
+            },
+        ]
+    }
 
+    const all_table_rows = ShopList.map((shopitem, index) => {
+      return (<tr key={index}>
+                <td>{shopitem.name}</td>
+                <td>{shopitem.quantity}</td>
+                <td>{shopitem.price.toFixed(2)}</td>
+                <td><button type="submit" className="ui button red">Delete</button></td>
+              </tr>);});
     return (
         <div class="row">
-            <label>
-				My Cart 
-			</label>
+            <h1 className="flexHeader">My Cart</h1>
             <div class="form-group col-sm-12 col-md-12">
                 <div class="col-sm-7 col-md-7">
                     <table id="cartTable">
                         <thead>
                             <tr>
-                                <th>Quantity</th>
                                 <th>Item</th>
+                                <th>Quantity</th>
                                 <th>Price</th>
-                                <th>Edit</th>
                                 <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody id="cartDataBody">
-        
+                            {props.length===0? 
+                                <tr>
+                                <td><center>No items in the shopping cart. </center></td>
+                                </tr>:
+                                all_table_rows}
                         </tbody>
                     </table>
                 </div>
@@ -36,7 +74,7 @@ const ShoppingCart = () => {
                             </tr>
                         </thead>
                         <tbody id="totalBody">
-        
+                            {shoppingList.length!==0 && <td>${totalPrice.toFixed(2)}</td>
                         </tbody>
                     </table>
                 </div>
